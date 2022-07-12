18dabb4d9ba566f109a45b77d09342c1a9a52be9
Dipali <dipssonawane123@gmail.com>
2022-07-12 23:06:24 +0530

    Modified: User registration using custom exception program is modified

--- src/main/java/com/regex/UserInputValidationUtil.java
+++ src/main/java/com/regex/UserInputValidationUtil.java
@@ -23,29 +23,20 @@ import java.util.regex.Pattern;
 public class UserInputValidationUtil {
     boolean result = false;
 
-    /**
-     * Creating isValidFirstName to validate the first name given by user using regex
-     *
-     * @param firstName - Taking the input given by user
-     * @return - true or false
-     */
+    // Method isValidFirstName to validate the first name given by user using regex
     public boolean isValidFirstName(String firstName) throws InvalidFirstNameException {
         try {
-            /**
-             * Declaring regex pattern to check the firstName
-             */
+
+             // Declaring regex pattern to check the firstName
             String firstNameRegex = "^[A-Z]{1}[a-z]{2,}$";
-            /**
-             * Compiling the regex
-             */
+
+             // Compiling the regex
             Pattern pattern = Pattern.compile(firstNameRegex);
-            /**
-             * Checking fot the pattern match
-             */
+
+             // Checking fot the pattern match
+
             Matcher matcher = pattern.matcher(firstName);
-            /**
-             * Returns matches result
-             */
+
             result = matcher.matches();
             if (!result) {
                 throw new InvalidFirstNameException("Please enter valid input.");
@@ -55,31 +46,13 @@ public class UserInputValidationUtil {
         }
         return result;
     }
-
-
-    /**
-     * Creating isValidLastName to validate the last name given by user using regex
-     *
-     * @param lastName - Taking the input given by user
-     * @return - true or false
-     */
+    // method isValidLastName to validate the last name given by user using regex
     public boolean isValidLastName(String lastName) throws InvalidLastNameException {
         try {
-            /**
-             * Declaring regex pattern to check the lastName
-             */
+
             String lastNameRegex = "^[A-Z]{1}[a-z]{2,}$";
-            /**
-             * Compiling the regex
-             */
             Pattern pattern = Pattern.compile(lastNameRegex);
-            /**
-             * Checking fot the pattern match
-             */
             Matcher matcher = pattern.matcher(lastName);
-            /**
-             * Returns matches result
-             */
             result = matcher.matches();
             if (!result) {
                 throw new InvalidLastNameException("Please enter valid input.");
@@ -90,30 +63,12 @@ public class UserInputValidationUtil {
         return result;
     }
 
-
-    /**
-     * Creating isValidEmail to validate the email given by user using regex
-     *
-     * @param email - Taking the input given by user
-     * @return - true or false
-     */
+    //method isValidEmail to validate the email given by user using regex
     public boolean isValidEmail(String email) throws InvalidEmailException {
         try {
-            /**
-             * Declaring regex pattern to check the email
-             */
             String emailRegex = "^[a-zA-Z-9]+([._+-]?[0-9A-Za-z]+)*@[a-zA-Z0-9]+.[a-zA-Z]{2,4}([.][a-z]{2})?$";
-            /**
-             * Compiling the regex
-             */
             Pattern pattern = Pattern.compile(emailRegex);
-            /**
-             * Checking fot the pattern match
-             */
             Matcher matcher = pattern.matcher(email);
-            /**
-             * Returns matches result
-             */
             result = matcher.matches();
             if (!result) {
                 throw new InvalidEmailException("Please enter valid input.");
@@ -124,29 +79,12 @@ public class UserInputValidationUtil {
         return result;
     }
 
-    /**
-     * Creating isValidMobileNumber to validate the mobile format given by user using regex
-     *
-     * @param mobileNumber - Taking the input given by user
-     * @return - true or false
-     */
+ // method isValidMobileNumber to validate the mobile format given by user using regex
     public boolean isValidMobileNumber(String mobileNumber) throws InvalidMobileNumberException {
         try {
-            /**
-             * Declaring regex pattern to check the mobile number
-             */
             String mobileNumberRegex = "^[0-9]{2}[\\s][0-9]{10}$";
-            /**
-             * Compiling the regex
-             */
             Pattern pattern = Pattern.compile(mobileNumberRegex);
-            /**
-             * Checking fot the pattern match
-             */
             Matcher matcher = pattern.matcher(mobileNumber);
-            /**
-             * Returns matches result
-             */
             result = matcher.matches();
             if (!result) {
                 throw new InvalidMobileNumberException("Please enter valid input.");
@@ -157,30 +95,14 @@ public class UserInputValidationUtil {
         return result;
     }
 
-    /**
-     * Creating isValidPassword to validate the password given by user using regex
-     *
-     * @param password - Taking the input given by user
-     * @return - true or false
-     */
+    //method isValidPassword to validate the password given by user using regex
     public boolean isValidPassword(String password) throws InvalidPasswordException {
         try {
-            /**
-             * Declaring regex pattern to check the password
-             */
             String passwordRegex = "^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9]*" +
                     "[\\@\\#\\^\\!\\$\\%\\&\\?][a-zA-Z0-9]*$";
-            /**
-             * Compiling the regex
-             */
+
             Pattern pattern = Pattern.compile(passwordRegex);
-            /**
-             * Checking fot the pattern match
-             */
             Matcher matcher = pattern.matcher(password);
-            /**
-             * Returns matches result
-             */
             result = matcher.matches();
             if (!result) {
                 throw new InvalidPasswordException("Please enter valid input.");
